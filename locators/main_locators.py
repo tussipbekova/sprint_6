e2b9cdc794663f28fc0be3375e43c8b987636984
Saya Tussipbekova <tussipbekova@gmail.com>
2023-12-13 23:00:00 +0600

    Added order in middle page tests

--- locators/main_locators.py
+++ locators/main_locators.py
@@ -2,7 +2,11 @@ from selenium.webdriver.common.by import By
 
 training_simulator = [By.XPATH, ".//div[text()='Учебный тренажер']"]
 
+cookies = [By.ID, 'rcc-confirm-button']
+
 header_order_button = [By.XPATH, ".//div[@class = 'Header_Nav__AGCXC'] / button[@class = 'Button_Button__ra12g']"]
 
 order_form_1_header = [By.XPATH, ".//div[@class = 'Order_Header__BZXOb']"]
+
+middle_order_button = [By.XPATH, ".//div[@class = 'Home_FinishButton__1_cWm'] / button[text() = 'Заказать']"]
 
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-from locators.main_locators import header_order_button, training_simulator, order_form_1_header
+from locators.main_locators import header_order_button, training_simulator, order_form_1_header, middle_order_button, \
+    cookies
 
 
 class MainPage:
@@ -15,5 +16,11 @@ class MainPage:
     def click_header_order_button(self):
         self.driver.find_element(*header_order_button).click()
 
+    def click_order_in_middle_page(self):
+        self.driver.find_element(*middle_order_button).click()
+
     def wait_for_load_order_form_1_header(self):
         WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(order_form_1_header))
+
+    def click_cookies(self):
+        self.driver.find_element(*cookies).click()
